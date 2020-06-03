1878658796d865028f2e5696a1bf6e267b565f58
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-06-03 17:42:06 +0200

    adding post method to find another city

--- src/main/java/pl/skrzypekjan/weatherapp/controller/WeatherController.java
+++ src/main/java/pl/skrzypekjan/weatherapp/controller/WeatherController.java
@@ -2,22 +2,35 @@ package pl.skrzypekjan.weatherapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import pl.skrzypekjan.weatherapp.model.WeatherAll;
 import pl.skrzypekjan.weatherapp.service.WeatherService;
 
 @Controller
 public class WeatherController {
     private WeatherService service;
+    private WeatherAll weatherAll;
 
-    public WeatherController(WeatherService service) {
+    public WeatherController(WeatherService service, WeatherAll weatherAll) {
         this.service = service;
+        this.weatherAll = weatherAll;
     }
 
-    @GetMapping("/{city}")
-    public String getWeatherByCity(Model model, @PathVariable String city){
-        model.addAttribute("weather", service.getWether(city));
+    @GetMapping
+    public String getWeatherByCity(Model model){
+        model.addAttribute("weather", service.getWeather("Gdańsk"));
         return "weather";
+    }
+
+    @PostMapping
+    public String setCity(Model model, String city){
+        try {
+            model.addAttribute("weather", service.getWeather(city));
+            return "weather";
+        } catch (HttpClientErrorException e){
+            System.out.println(e);
+        }
+        return "error";
     }
 }
